3524b43feb03b8cf57cb472f7de13b6f4ddf5d1e
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-03-02 20:29:19 +0530

    Fixed version incompatible issue

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,11 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-// module.exports = wrapWithReanimatedMetroConfig(config);
